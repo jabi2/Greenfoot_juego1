a0f5271b04fc9b72558d4be08e4fd4a1c3e2984a
jabi2 <boc21211@uvg.edu.gt>
2021-07-24 23:29:50 -0600

    Create topo.java

---
+++ juego/topo.java
@@ -0,0 +1,71 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class topo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class topo extends Actor
+{
+    /**
+     * Act - do whatever the topo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        moveAndTurn();
+        eat();
+    }
+    
+    public void moveAndTurn()
+    {
+        setImage("gold-ball.png");
+        
+        int a;
+        a = getX();
+        
+        int b;
+        b = getY();
+        
+        if (Greenfoot.isKeyDown("up"))
+        {
+            setLocation(a,b-1);
+            //move (1);
+        }
+        if (Greenfoot.isKeyDown("left"))
+        {
+            setLocation(a-1,b);
+            
+        }
+        
+        if (Greenfoot.isKeyDown("right"))
+        {
+            setLocation(a+1,b);
+        }
+        
+        if (Greenfoot.isKeyDown("down"))
+        {
+            setLocation(a,b+1);
+        }
+    }
+    
+    public void eat()
+    {
+        Actor worm;
+        worm = getOneObjectAtOffset(0, 0, Worm.class);
+        
+    
+        //imagen = getImage()
+        if (worm != null)
+            if (Greenfoot.isKeyDown("space"))
+            {
+                setImage("red-draught.png");
+            
+            
+                World world;
+                world = getWorld();
+                world.removeObject(worm);
+            }// Add your action code here.
+    }   
+}
